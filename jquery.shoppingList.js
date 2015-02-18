e0d1c9ff7def9d4e01f7e34f4ae14025b1a1e8e9
biche <kevin87100@gmail.com>
2015-02-18 14:15:23 +0100

    test

--- jquery.shoppingList.js
+++ jquery.shoppingList.js
@@ -15,58 +15,59 @@
 				
 			var obj = $(this);
 			
-			// Empêcher la sélection des éléments à la sourirs (meilleure gestion du drag & drop)
+			// Empï¿½cher la sï¿½lection des ï¿½lï¿½ments ï¿½ la sourirs (meilleure gestion du drag & drop)
 			var _preventDefault = function(evt) { evt.preventDefault(); };
 			$("li").bind("dragstart", _preventDefault).bind("selectstart", _preventDefault);
 
 			// Initialisation du composant "sortable"
 			$(obj).sortable({
 				axis: "y", // Le sortable ne s'applique que sur l'axe vertical
-				containment: ".shoppingList", // Le drag ne peut sortir de l'élément qui contient la liste
-				handle: ".item", // Le drag ne peut se faire que sur l'élément .item (le texte)
-				distance: 10, // Le drag ne commence qu'à partir de 10px de distance de l'élément
-				// Evenement appelé lorsque l'élément est relaché
+				containment: ".shoppingList", // Le drag ne peut sortir de l'ï¿½lï¿½ment qui contient la liste
+				handle: ".item", // Le drag ne peut se faire que sur l'ï¿½lï¿½ment .item (le texte)
+				distance: 10, // Le drag ne commence qu'ï¿½ partir de 10px de distance de l'ï¿½lï¿½ment
+				// Evenement appelï¿½ lorsque l'ï¿½lï¿½ment est relachï¿½
 				stop: function(event, ui){
 					// Pour chaque item de liste
 					$(obj).find("li").each(function(){
 						// On actualise sa position
 						index = parseInt($(this).index()+1);
-						// On la met à jour dans la page
+						// On la met ï¿½ jour dans la page
 						$(this).find(".count").text(index);
 					});
 				}
 			});
-			
-			// On ajoute l'élément Poubelle à notre liste
+			$("ul").dragable;
+			/*
+			// On ajoute l'ï¿½lï¿½ment Poubelle ï¿½ notre liste
 			$(obj).after("<div class='trash'>Trash</div>");
 			// On ajoute un petit formulaire pour ajouter des items
 			$(obj).after("<div class='add'><input class='addValue' /> <input type='button' value='Add' class='addBtn' /></div>");
-					
+				*/	
 			// Action de la poubelle
 			// Initialisation du composant Droppable
-			$(".trash").droppable({
-				// Lorsque l'on relache un élément sur la poubelle
+			/*$(".trash").droppable({
+				// Lorsque l'on relache un ï¿½lï¿½ment sur la poubelle
 				drop: function(event, ui){
-					// On retire la classe "hover" associée au div .trash
+					// On retire la classe "hover" associï¿½e au div .trash
 					$(this).removeClass("hover");
-					// On ajoute la classe "deleted" au div .trash pour signifier que l'élément a bien été supprimé
+					// On ajoute la classe "deleted" au div .trash pour signifier que l'ï¿½lï¿½ment a bien ï¿½tï¿½ supprimï¿½
 					$(this).addClass("deleted");
-					// On affiche un petit message "Cet élément a été supprimé" en récupérant la valeur textuelle de l'élément relaché
+					// On affiche un petit message "Cet ï¿½lï¿½ment a ï¿½tï¿½ supprimï¿½" en rï¿½cupï¿½rant la valeur textuelle de l'ï¿½lï¿½ment relachï¿½
 					$(this).text(ui.draggable.find(".item").text()+" removed !");
-					// On supprimer l'élément de la page, le setTimeout est un fix pour IE (http://dev.jqueryui.com/ticket/4088)
+					// On supprimer l'ï¿½lï¿½ment de la page, le setTimeout est un fix pour IE (http://dev.jqueryui.com/ticket/4088)
 					setTimeout(function() { ui.draggable.remove(); }, 1);
 					
-					// On retourne à l'état originel de la poubelle après 2000 ms soit 2 secondes
+					// On retourne ï¿½ l'ï¿½tat originel de la poubelle aprï¿½s 2000 ms soit 2 secondes
 					elt = $(this);
 					setTimeout(function(){ elt.removeClass("deleted"); elt.text("Trash"); }, 2000);
 				},
-				// Lorsque l'on passe un élément au dessus de la poubelle
+				// Lorsque l'on passe un ï¿½lï¿½ment au dessus de la poubelle
 				over: function(event, ui){
 					// On ajoute la classe "hover" au div .trash
 					$(this).addClass("hover");
-					// On cache l'élément déplacé
+					// On cache l'ï¿½lï¿½ment dï¿½placï¿½
 					ui.draggable.hide();
-					// On indique via un petit message si l'on veut bien supprimer cet élément
+					// On indique via un petit message si l'on veut bien supprimer cet ï¿½lï¿½ment
 					$(this).text("Remove "+ui.draggable.find(".item").text());
 					// On change le curseur
 					$(this).css("cursor", "pointer");
@@ -75,73 +76,73 @@
 				out: function(event, ui){
 					// On retire la classe "hover" au div .trash
 					$(this).removeClass("hover");
-					// On réaffiche l'élément déplacé
+					// On rï¿½affiche l'ï¿½lï¿½ment dï¿½placï¿½
 					ui.draggable.show();
-					// On remet le texte par défaut
+					// On remet le texte par dï¿½faut
 					$(this).text("Trash");
-					// Ainsi que le curseur par défaut
+					// Ainsi que le curseur par dï¿½faut
 					$(this).css("cursor", "normal");
 				}
 			})
-						
+			*/			
 			/*
 			* Ajouter les controles sur le bouton "ajouter"
 			*
 			* @Return void
 			*/
-			
+			/*
 			// Bouton ajouter
 			$(".addBtn").click(function(){
 				// Si le texte n'est pas vide
 				if($(".addValue").val() != "")
 				{
-					// On ajoute un nouvel item à notre liste
+					// On ajoute un nouvel item ï¿½ notre liste
 					$(obj).append('<li>'+$(".addValue").val()+'</li>');
-					// On réinitialise le champ de texte pour l'ajout
+					// On rï¿½initialise le champ de texte pour l'ajout
 					$(".addValue").val("");
-					// On ajoute les contrôles à notre nouvel item
+					// On ajoute les contrï¿½les ï¿½ notre nouvel item
 					addControls($(obj).find("li:last-child"));
 				}
 			})
-			// On autorise également la validation de la saisie d'un nouvel item par pression de la touche entrée
+			// On autorise ï¿½galement la validation de la saisie d'un nouvel item par pression de la touche entrï¿½e
 			$(".addValue").live("keyup", function(e) {
 				if(e.keyCode == 13) {
-					// On lance l'évènement click associé au bouton d'ajout d'item
+					// On lance l'ï¿½vï¿½nement click associï¿½ au bouton d'ajout d'item
 					$(".addBtn").trigger("click");
 				}
 			});
-			
-			// Pour chaque élément trouvé dans la liste de départ
+			*/
+			// Pour chaque ï¿½lï¿½ment trouvï¿½ dans la liste de dï¿½part
 			$(obj).find("li").each(function(){
-				// On ajoute les contrôles
+				// On ajoute les contrï¿½les
 				addControls($(this));
 			});
 
 		});
 				
 		/*
-		* Fonction qui ajoute les contrôles aux items
-		* @Paramètres
-		*  - elt: élément courant (liste courante)
+		* Fonction qui ajoute les contrï¿½les aux items
+		* @Paramï¿½tres
+		*  - elt: ï¿½lï¿½ment courant (liste courante)
 		*
 		* @Return void
 		*/
 		
 		function addControls(elt)
 		{
-			// On ajoute en premier l'élément textuel
+			// On ajoute en premier l'ï¿½lï¿½ment textuel
 			$(elt).html("<span class='item'>"+$(elt).text()+"</item>");
-			// Puis l'élément de position
+			// Puis l'ï¿½lï¿½ment de position
 			$(elt).prepend('<span class="count">'+parseInt($(elt).index()+1)+'</span>');
-			// Puis l'élément d'action (élément acheté)
+			// Puis l'ï¿½lï¿½ment d'action (ï¿½lï¿½ment achetï¿½)
 			$(elt).prepend('<span class="check unchecked"></span>');
 			
-			// Au clic sur cet élément
+			// Au clic sur cet ï¿½lï¿½ment
 			$(elt).find(".check").click(function(){
-				// On alterne la classe de l'item (le <li>), le CSS associé fera que l'élément sera barré
+				// On alterne la classe de l'item (le <li>), le CSS associï¿½ fera que l'ï¿½lï¿½ment sera barrï¿½
 				$(this).parent().toggleClass("bought");
 				
-				// Si cet élément est acheté
+				// Si cet ï¿½lï¿½ment est achetï¿½
 				if($(this).parent().hasClass("bought"))
 					// On modifie la classe en ajoutant la classe "checked"
 					$(this).removeClass("unchecked").addClass("checked");
@@ -153,23 +154,23 @@
 			
 			// Au double clic sur le texte
 			$(elt).find(".item").dblclick(function(){
-				// On récupère sa valeur
+				// On rï¿½cupï¿½re sa valeur
 				txt = $(this).text();
 				// On ajoute un champ de saisie avec la valeur
 				$(this).html("<input value='"+txt+"' />");
-				// On la sélectionne par défaut
+				// On la sï¿½lectionne par dï¿½faut
 				$(this).find("input").select();
 			})
 			
 			// Lorsque l'on quitte la zone de saisie du texte
 			$(elt).find(".item input").live("blur", function(){
-				// On récupère la valeur du champ de saisie
+				// On rï¿½cupï¿½re la valeur du champ de saisie
 				txt = $(this).val();
-				// On insère dans le <li> la nouvelle valeur textuelle
+				// On insï¿½re dans le <li> la nouvelle valeur textuelle
 				$(this).parent().html(txt);
 			})
 			
-			// On autorise la même action lorsque l'on valide par la touche entrée
+			// On autorise la mï¿½me action lorsque l'on valide par la touche entrï¿½e
 			$(elt).find(".item input").live("keyup", function(e) {
 				if(e.keyCode == 13) {
 					$(this).trigger("blur");
