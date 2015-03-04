3948b9bbfdb3d13c69475cfb82fa89af61185e95
Fabien <fiba.lol@gmail.com>
2015-03-04 14:15:59 +0100

    CA MARCH 2

--- mapage.php
+++ mapage.php
@@ -2,11 +2,3 @@
 	
 	echo "test";
 ?>
-<?php
-    			$contenu = $_POST["contenu"];
-				$fichier_a_ouvrir = fopen ("editable.html", "w+");
-				//On écrit dans le fichier
-				fwrite($fichier_a_ouvrir, $contenu);
-				//On ferme la connexion
-				fclose ($fichier_a_ouvrir);
-    		?>
