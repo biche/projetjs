9d353c52916f31627cd48e60d0caa4857c35a125
Fabien <fiba.lol@gmail.com>
2015-03-17 10:53:30 +0100

    Création bootstrap

--- creation.php
+++ creation.php
@@ -5,81 +5,110 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="import" href="editable.html">
+        <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all" />
+        <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     	<script src="js/jquery-1.4.2.min.js"></script>
-    	<script type="text/javascript">
-    	</script>
+    	<script src="js/bootstrap.js"></script>
+    	<script src="js/bootstrap.min.js"></script>
+    	<script type="text/javascript" src="js/bootstrap-filestyle.js"></script>
         <script src="edit.js" type="text/javascript" charset="utf-8"></script>
     </head>
 	<body onload="InitEditable()">
-		<br /> 
-		<!-- Iframe permet la saisie et l'interprétation du HTML  -->
-		<iframe id="editor" src="template.php" style="width: 80%;height: 200%;"> </iframe>
-		<br /><br />
-		<br />
 		<!-- FormatDoc permettant de la mise en forme du code HTML  -->
-		<button onclick="formatDoc('bold');">Gras</button>
-		<button onclick="formatDoc('italic');">Italique</button>
-		<button onclick="formatDoc('underline');">Souligné</button>
-		<select onchange="formatDoc('formatblock',this[this.selectedIndex].value);">
-			<option selected>Format</option>
-			<option value="h1">Titre 1 &lt;h1&gt;</option>
-			<option value="h2">Titre 2 &lt;h2&gt;</option>
-			<option value="h3">Titre 3 &lt;h3&gt;</option>
-			<option value="h4">Titre 4 &lt;h4&gt;</option>
-			<option value="h5">Titre 5 &lt;h5&gt;</option>
-			<option value="h6">Sous-titre &lt;h6&gt;</option>
-			<option value="p">Paragraphe &lt;p&gt;</option>
-		</select>
-		<select onchange="formatDoc('fontname',this[this.selectedIndex].value);">
-			<option selected>Police de caractère</option>
-			<option>Arial</option>
-			<option>Arial Black</option>
-			<option>Courier New</option>
-			<option>Times New Roman</option>
-		</select>
-		<select onchange="formatDoc('fontsize',this[this.selectedIndex].value);">
-			<option class="heading" selected>Taille</option>
-			<option value="1">Très petit</option>
-			<option value="2">Petit</option>
-			<option value="3">Normal</option>
-			<option value="4">Moyen</option>
-			<option value="5">Gros</option>
-			<option value="6">Très gros</option>
-			<option value="7">Maximum</option>
-		</select>
-		<select onchange="formatDoc('forecolor',this[this.selectedIndex].value);">
-			<option class="heading" selected>Couleur du texte</option>
-			<option value="red">Rouge</option>
-			<option value="blue">Bleu</option>
-			<option value="green">Vert</option>
-			<option value="black">Noir</option>
-		</select>
-		<select onchange="formatDoc('backcolor',this[this.selectedIndex].value);">
-			<option class="heading" selected>Couleur de l'arrière plan</option>
-			<option value="red">rouge</option>
-			<option value="green">Vert</option>
-			<option value="black">Noir</option>
-			<option value="white">Blanc</option>
-		</select>
-		<button onclick="formatDoc('justifyleft');">Aligner à gauche</button>
-		<button onclick="formatDoc('justifycenter');">Centrer</button>
-		<button onclick="formatDoc('justifyright');">Aligner à droite</button>
-		<button onclick="formatDoc('insertorderedlist');">Liste numérotée</button>
-		<button onclick="formatDoc('insertunorderedlist');">Liste à puces</button>
-		<button onclick="formatDoc('cut');">Couper</button>
-		<button onclick="formatDoc('copy');">Copier</button>
-		<button onclick="formatDoc('paste');">Coller</button>
-		<button onclick="formatDoc('undo');">Annuler</button>
-		<button onclick="formatDoc('redo');">Refaire</button>
+		<div style="margin:50px;">
+			<div class="btn-group" role="group" aria-label="1">
+			  	<button type="button" class="btn btn-default" onclick="formatDoc('bold');"><span class="fa fa-fw fa-bold"></span></button>
+			 	<button type="button" class="btn btn-default" onclick="formatDoc('italic');"><span class="fa fa-fw fa-italic"></span></button>
+				<button type="button" class="btn btn-default" onclick="formatDoc('underline');"><span class="fa fa-fw fa-underline"></span></button>
+			</div>
+
+
+
+			<div class="btn-group" role="group" aria-label="2">
+			  	<button type="button" class="btn btn-default" onclick="formatDoc('justifyleft');"><span class="fa fa-fw fa-align-left"></span></button>
+				<button type="button" class="btn btn-default" onclick="formatDoc('justifycenter');"><span class="fa fa-fw fa-align-center"></span></button>
+				<button type="button" class="btn btn-default" onclick="formatDoc('justifyright');"><span class="fa fa-fw fa-align-right"></span></button>
+			</div>
+
+			<div class="btn-group" role="group" aria-label="3">
+				<button type="button" class="btn btn-default" onclick="formatDoc('insertorderedlist');"><span class="fa fa-fw fa-list-ol"></span></button>
+				<button type="button" class="btn btn-default" onclick="formatDoc('insertunorderedlist');"><span class="fa fa-fw fa-list-ul"></span></button>
+			</div>
+
+			<div class="btn-group" role="group" aria-label="4">
+				<button type="button" class="btn btn-default" onclick="formatDoc('cut');"><span class="fa fa-fw fa-cut"></span></button>
+				<button type="button" class="btn btn-default" onclick="formatDoc('copy');"><span class="fa fa-fw fa-copy"></span></button>
+				<button type="button" class="btn btn-default" onclick="formatDoc('paste');"><span class="fa fa-fw fa-paste"></span></button>
+			</div>
+
+			<div class="btn-group" role="group" aria-label="5">
+				<button type="button" class="btn btn-default" onclick="formatDoc('undo');"><span class="fa fa-fw fa-undo"></span></button>
+				<button type="button" class="btn btn-default" onclick="formatDoc('redo');"><span class="fa fa-fw fa-repeat"></span></button>
+			</div>
+
+			<div class="form-group form-group-sm">
+				<div class="col-xs-2">
+					<select class="form-control" onchange="formatDoc('formatblock',this[this.selectedIndex].value);">
+						<option selected>Format</option>
+						<option value="h1">Titre 1 &lt;h1&gt;</option>
+						<option value="h2">Titre 2 &lt;h2&gt;</option>
+						<option value="h3">Titre 3 &lt;h3&gt;</option>
+						<option value="h4">Titre 4 &lt;h4&gt;</option>
+						<option value="h5">Titre 5 &lt;h5&gt;</option>
+						<option value="h6">Sous-titre &lt;h6&gt;</option>
+						<option value="p">Paragraphe &lt;p&gt;</option>
+					</select>
+					<select onchange="formatDoc('fontname',this[this.selectedIndex].value);">
+						<option selected>Police de caractère</option>
+						<option>Arial</option>
+						<option>Arial Black</option>
+						<option>Courier New</option>
+						<option>Times New Roman</option>
+					</select>
+					<select onchange="formatDoc('fontsize',this[this.selectedIndex].value);">
+						<option class="heading" selected>Taille</option>
+						<option value="1">Très petit</option>
+						<option value="2">Petit</option>
+						<option value="3">Normal</option>
+						<option value="4">Moyen</option>
+						<option value="5">Gros</option>
+						<option value="6">Très gros</option>
+						<option value="7">Maximum</option>
+					</select>
+					<select onchange="formatDoc('forecolor',this[this.selectedIndex].value);">
+						<option class="heading" selected>Couleur du texte</option>
+						<option value="red">Rouge</option>
+						<option value="blue">Bleu</option>
+						<option value="green">Vert</option>
+						<option value="black">Noir</option>
+					</select>
+					<select onchange="formatDoc('backcolor',this[this.selectedIndex].value);">
+						<option class="heading" selected>Couleur de l'arrière plan</option>
+						<option value="red">rouge</option>
+						<option value="green">Vert</option>
+						<option value="black">Noir</option>
+						<option value="white">Blanc</option>
+					</select>
+				</div>
+			</div>
+			
+			<!-- Iframe permet la saisie et l'interprétation du HTML  -->
+			<iframe id="editor" src="template.php" style="width: 80%;height: 200%;"> </iframe>
+		
 		<!-- Upload d'image -->
-		<form action="#" method="post" enctype="multipart/form-data">
-    		Select image to upload:
-    		<input type="file" name="fileToUpload" id="fileToUpload">
-    		<input type="submit" value="Upload Image" name="submit">
+		<form class="form-horizontal" action="#" method="post" enctype="multipart/form-data">
+	    		<div  class="col-xs-4">
+	    			<input type="file" name="fileToUpload" id="fileToUpload" class="filestyle">
+					<button type="submit" class="btn btn-success" style="margin-left:20px;"><i class="fa fa-fw fa-upload"></i> Uploader</button>
+					<div class="btn-group" role="group" aria-label="5">
+						<button title="Mettre image" type="button" class="btn btn-default" onclick="formatDoc('insertImage','upload/<?php if(isset($_FILES["fileToUpload"])){echo basename( $_FILES["fileToUpload"]["name"]);} ?>');">
+							<i class="fa fa-fw fa-send"></i> Mettre l'image
+						</button>
+					</div>
+				</div>
 		</form>
-		<!-- Envoi de l'image -->
-		<button onclick="formatDoc('insertImage','upload/<?php if(isset($_FILES["fileToUpload"])){echo basename( $_FILES["fileToUpload"]["name"]);} ?>');">Envoi de l'image</button>
-<?php
+	<?php
 	if(isset($_FILES["fileToUpload"])){
 		$target_dir = "upload/";
 		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -127,7 +156,9 @@
 ?>
 		<label for="nomPage">Nom de la page : </label>
 		<input name="nomPage" id="nom" type="text" placeholder="coucou" required />
-		<button onclick="creerPage();">Envoyer</button>
+		<div class="btn-group" role="group" aria-label="5">
+			<button style="margin-left:200px;" type="button" class="btn btn-success" onclick="creerPage();"><span class="fa fa-fw fa-send"></span> Envoyer</button>
+		</div>
 		</form>
 	</body>
 </html>
