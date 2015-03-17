6bcb93fd0b1f3f89d73ce589878b7fb25500d589
Fabien <fiba.lol@gmail.com>
2015-03-17 10:53:51 +0100

    basard

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <link rel="import" href="editable.html">
-    	<script src="js/jquery-1.4.2.min.js"></script>
-    	<script type="text/javascript">
-    	</script>
-        <script src="edit.js" type="text/javascript" charset="utf-8"></script>
-    </head>
-	<body spellcheck="false" contenteditable="true"><h1>Test</h1><button onclick="sendPageName();">Modifier</button>
-		<button onclick="document.location='creation.php';">Créer</button>
-
+	<head></head><body spellcheck="false" contenteditable="true"><h1>Titre de la Convocation</h1><img src="upload/Best download.jpg">
 </body>
 </html>
